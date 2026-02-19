cc77d32308114bbf35ce44ba1a45728769ce3634
Rodolfo Gonzalez <rudynoot123@gmail.com>
2026-02-18 19:21:52 -0600

    Tested transcription pulling

--- transcription/transcript.py
+++ transcription/transcript.py
@@ -1,5 +1,10 @@
-from youtube_transcript_api import YoutubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi
+import json 
 
-ytt_api = YoutubeTranscriptApi()
-ytt_api.fetch(video_id)#Have to get youtube video
+ytt_api = YouTubeTranscriptApi()
+transcript = ytt_api.fetch("3TKJDNcrBXQ")
 
+
+with open("transcript.txt","w") as file:
+    for snippet in transcript:
+        file.write(snippet.text + "\n")
